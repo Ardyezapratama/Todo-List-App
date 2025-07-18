e173fe24ebf564b08a3d85205a68e2fee8e05051
Ardyezapratama <ardyezapratama@gmail.com>
2025-07-18 15:30:31 +0800

    docs: implementing onlyGuestMiddleware
    modify web.php to implementing onlyGuestMiddleware to login route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\OnlyGuestMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +9,7 @@ Route::get('/', function () {
 });
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/login', 'login');
-    Route::post('/login', 'doLogin');
+    Route::get('/login', 'login')->middleware([OnlyGuestMiddleware::class]);
+    Route::post('/login', 'doLogin')->middleware(OnlyGuestMiddleware::class);
     Route::post('/logout', 'doLogout');
 });
