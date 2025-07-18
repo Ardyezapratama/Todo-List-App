7030f6c5c6b0adfd6aac45952d324f15e63262b0
Ardyezapratama <ardyezapratama@gmail.com>
2025-07-18 17:00:35 +0800

    docs: modify home route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\OnlyGuestMiddleware;
 use App\Http\Middleware\OnlyMemberMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'home']);
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/login', 'login')->middleware([OnlyGuestMiddleware::class]);
