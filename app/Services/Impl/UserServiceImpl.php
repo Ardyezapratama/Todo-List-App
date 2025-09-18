b79da8893bf19a640124994e2da5836aa4caa732
Ardyezapratama <ardyezapratama@gmail.com>
2025-09-18 08:53:54 +0800

    Fix login user

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -3,19 +3,18 @@
 namespace App\Services\Impl;
 
 use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
 
 class UserServiceImpl implements UserService
 {
-    private array $user = [
-        'ezapratama' => 'rahasia'
-    ];
-    public function login(string $user, string $password): bool
-    {
-        if (!isset($this->user[$user])) {
-            return false;
-        }
 
-        $correctPassword = $this->user[$user];
-        return $password === $correctPassword;
+    public function login(string $email, string $password): bool
+    {
+        return Auth::attempt([
+            "email" => $email,
+            "password" => $password
+        ]);
+
     }
+
 }
